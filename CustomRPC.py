33dbd0a2856986a4142b60435ae74f5b4a8b1e44
Ondra907 <ondra907@email.cz>
2022-11-06 15:10:26 +0100

    Add files via upload

--- CustomRPC.py
+++ CustomRPC.py
@@ -76,24 +76,78 @@ Thank you for using CustomRPC!
 © CustomRPC 2022 | By Ondra907
 """)
 
+def error():
+    os.system("cls")
+    print(color.light, Fore.RED     + """
+------------------------------------------------------------------------------------------- 
+
+ ▄████▄   █    ██   ██████ ▄▄▄█████▓ ▒█████   ███▄ ▄███▓          ██▀███   ██▓███   ▄████▄  
+▒██▀ ▀█   ██  ▓██▒▒██    ▒ ▓  ██▒ ▓▒▒██▒  ██▒▓██▒▀█▀ ██▒         ▓██ ▒ ██▒▓██░  ██▒▒██▀ ▀█  
+▒▓█    ▄ ▓██  ▒██░░ ▓██▄   ▒ ▓██░ ▒░▒██░  ██▒▓██    ▓██░         ▓██ ░▄█ ▒▓██░ ██▓▒▒▓█    ▄ 
+▒▓▓▄ ▄██▒▓▓█  ░██░  ▒   ██▒░ ▓██▓ ░ ▒██   ██░▒██    ▒██          ▒██▀▀█▄  ▒██▄█▓▒ ▒▒▓▓▄ ▄██▒
+▒ ▓███▀ ░▒▒█████▓ ▒██████▒▒  ▒██▒ ░ ░ ████▓▒░▒██▒   ░██▒         ░██▓ ▒██▒▒██▒ ░  ░▒ ▓███▀ ░
+░ ░▒ ▒  ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░  ▒ ░░   ░ ▒░▒░▒░ ░ ▒░   ░  ░         ░ ▒▓ ░▒▓░▒▓▒░ ░  ░░ ░▒ ▒  ░
+  ░  ▒   ░░▒░ ░ ░ ░ ░▒  ░ ░    ░      ░ ▒ ▒░ ░  ░      ░           ░▒ ░ ▒░░▒ ░       ░  ▒   
+░         ░░░ ░ ░ ░  ░  ░    ░      ░ ░ ░ ▒  ░      ░              ░░   ░ ░░       ░        
+░ ░         ░           ░               ░ ░         ░               ░              ░ ░      
+░                                                                                  ░        
+
+https://github.com/Ondra9071/CustomRPC
+-------------------------------------------------------------------------------------------
+
+An error has occurred, restart the program.
+""")
+
 main()
 
-# ================================== INPUTS ================================== #
+# ================================== PICK ================================== #
 
-inID = input("Discord App ID: ")
-inState = input("State: ")
-inLargeImg = input("Large Image: ")
-inLargeImgText = input("Large Image Text: ")
+rpcType = input("""What type of RPC do you want?
+                
+1 = Image, State
+2 = Image, State & Button
 
-# ================================== RPC GENERATOR ================================== #
-client_id = inID
-RPC = Presence(client_id)
-RPC.connect()
-start_time = time.time()
-RPC.update(state=inState, large_image=inLargeImg, large_text=inLargeImgText, start=start_time)
+: """)
 
+os.system("cls")
+
+# ================================== #1 RPC ================================== #
+
+if (rpcType == "1"):
+  inID = input("Discord App ID: ")
+  inState = input("State: ")
+  inLargeImg = input("Large Image: ")
+  inLargeImgText = input("Large Image Text: ")
+
+  client_id = inID
+  RPC = Presence(client_id)
+  RPC.connect()
+  start_time = time.time()
+  RPC.update(state=inState, large_image=inLargeImg, large_text=inLargeImgText, start=start_time)
+  
+# ================================== #2 RPC ================================== #
+elif (rpcType == "2"):
+  main()
+  inID = input("Discord App ID: ")
+  inState = input("State: ")
+  inLargeImg = input("Large Image: ")
+  inLargeImgText = input("Large Image Text: ")
+  ButtonText = input("Button Text: ")
+  ButtonURL = input("Button URL: ")
+  
+  client_id = inID
+  RPC = Presence(client_id)
+  RPC.connect()
+  start_time = time.time()
+  RPC.update(state=inState, large_image=inLargeImg, large_text=inLargeImgText, start=start_time, buttons = [{"label": ButtonText, "url": ButtonURL}])
+
+else:
+  error()
+  input("Press ENTER to close the program.")
+  
 # ================================== THE END ================================== #
 
+os.system()
 done()
 end = input()
 
